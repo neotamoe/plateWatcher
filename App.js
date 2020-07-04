2168944a07a926bd3f716e21c078eb1264656631
neotamoe <neota.moe@daugherty.com>
2020-07-04 13:12:43 -0500

    initial copy over of tutorial code

--- App.js
+++ App.js
@@ -1,114 +1,219 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// Code from: https://medium.com/zenofai/object-detection-in-react-native-app-using-aws-rekognition-ebd39b1270c2
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+ StyleSheet,
+ View,
+ Text,
+ TextInput,
+ Image,
+ ScrollView,
+ TouchableHighlight,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
+import ImagePicker from 'react-native-image-picker';
+import Amplify, {API} from 'aws-amplify';
+ 
+// Amplify configuration for API-Gateway
+// Amplify.configure({
+//  API: {
+//    endpoints: [
+//      {
+//        name: 'LabellingAPI',   //your api name
+//        endpoint:’<Endpoint-URL>’, //Your Endpoint URL
+//      },
+//    ],
+//  },
+// });
+ 
+class App extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     username: 'storeImage.png',
+     userId: '',
+     image: '',
+     capturedImage: '',
+     objectName: '',
+   };
+ }
+ 
+// It selects image from filesystem or capture from camera
+ captureImageButtonHandler = () => {
+   this.setState({
+     objectName: '',
+   });
+ 
+   ImagePicker.showImagePicker(
+     {title: 'Pick an Image', maxWidth: 800, maxHeight: 600},
+     response => {
+       console.log('Response = ', response);
+       if (response.didCancel) {
+         console.log('User cancelled image picker');
+       } else if (response.error) {
+         console.log('ImagePicker Error: ', response.error);
+       } else if (response.customButton) {
+         console.log('User tapped custom button: ', response.customButton);
+       } else {
+         // You can also display the image using data:
+         const source = {uri: 'data:image/jpeg;base64,' + response.data};
+         this.setState({
+           capturedImage: response.uri,
+           base64String: source.uri,
+         });
+       }
+     },
+   );
+ };
+ 
+// this method triggers when you click submit. If the image is valid then It will send the image to API Gateway. 
+ submitButtonHandler = () => {
+   if (
+     this.state.capturedImage == '' ||
+     this.state.capturedImage == undefined ||
+     this.state.capturedImage == null
+   ) {
+     alert('Please Capture the Image');
+   } else {
+     const apiName = 'LabellingAPI';
+     const path = '/storeimage';
+     const init = {
+       headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/x-amz-json-1.1',
+       },
+       body: JSON.stringify({
+         Image: this.state.base64String,
+         name: 'storeImage.png',
+       }),
+     };
+ 
+     API.post(apiName, path, init).then(response => {
+       if (JSON.stringify(response.Labels.length) > 0) {
+         this.setState({
+           objectName: response.Labels[0].Name,
+         });
+       } else {
+         alert('Please Try Again.');
+       }
+     });
+   }
+ };
+ 
+ render() {
+   if (this.state.image !== '') {
+   }
+   return (
+     <View style={styles.MainContainer}>
+       <ScrollView>
+         <Text
+           style={{
+             fontSize: 20,
+             color: '#000',
+             textAlign: 'center',
+             marginBottom: 15,
+             marginTop: 10,
+           }}>
+           Capture Image
+         </Text>
+         {this.state.capturedImage !== '' && (
+           <View style={styles.imageholder}>
+             <Image
+               source={{uri: this.state.capturedImage}}
+               style={styles.previewImage}
+             />
+           </View>
+         )}
+         {this.state.objectName ? (
+           <TextInput
+             underlineColorAndroid="transparent"
+             style={styles.TextInputStyleClass}
+             value={this.state.objectName}
+           />
+         ) : null}
+         <TouchableHighlight
+           style={[styles.buttonContainer, styles.captureButton]}
+           onPress={this.captureImageButtonHandler}>
+           <Text style={styles.buttonText}>Capture Image</Text>
+         </TouchableHighlight>
+ 
+         <TouchableHighlight
+           style={[styles.buttonContainer, styles.submitButton]}
+           onPress={this.submitButtonHandler}>
+           <Text style={styles.buttonText}>Submit</Text>
+         </TouchableHighlight>
+       </ScrollView>
+     </View>
+   );
+ }
+}
+ 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+ TextInputStyleClass: {
+   textAlign: 'center',
+   marginBottom: 7,
+   height: 40,
+   borderWidth: 1,
+   marginLeft: 90,
+   width: '50%',
+   justifyContent: 'center',
+   borderColor: '#D0D0D0',
+   borderRadius: 5,
+ },
+ inputContainer: {
+   borderBottomColor: '#F5FCFF',
+   backgroundColor: '#FFFFFF',
+   borderRadius: 30,
+   borderBottomWidth: 1,
+   width: 300,
+   height: 45,
+   marginBottom: 20,
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+ buttonContainer: {
+   height: 45,
+   flexDirection: 'row',
+   alignItems: 'center',
+   justifyContent: 'center',
+   marginBottom: 20,
+   width: '80%',
+   borderRadius: 30,
+   marginTop: 20,
+   marginLeft: 5,
+ },
+ captureButton: {
+   backgroundColor: '#337ab7',
+   width: 350,
+ },
+ buttonText: {
+   color: 'white',
+   fontWeight: 'bold',
+ },
+ horizontal: {
+   flexDirection: 'row',
+   justifyContent: 'space-around',
+   padding: 10,
+ },
+ submitButton: {
+   backgroundColor: '#C0C0C0',
+   width: 350,
+   marginTop: 5,
+ },
+ imageholder: {
+   borderWidth: 1,
+   borderColor: 'grey',
+   backgroundColor: '#eee',
+   width: '50%',
+   height: 150,
+   marginTop: 10,
+   marginLeft: 90,
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+ previewImage: {
+   width: '100%',
+   height: '100%',
+ },
 });
 
 export default App;
